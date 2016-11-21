b91ed33db2311eea0b0e514c8d0ac488dfb92a94
Dorian Kurzaj <doriank@hotmail.fr>
2016-11-21 18:04:17 +0100

    Contact the LDAP proxy through POST request instead of GET because specials chars were not supported easily

--- Internships/src/fr/insa/servlets/Connection.java
+++ Internships/src/fr/insa/servlets/Connection.java
@@ -1,6 +1,7 @@
 package fr.insa.servlets;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -12,42 +13,73 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/Connection")
 public class Connection extends HttpServlet {
 	public static final String VIEW = "/WEB-INF/connection.jsp";
 	private static final String CHAMP_NOM = "nom";
 	private static final String CHAMP_PASS = "motdepasse";
-	
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		this.getServletContext().getRequestDispatcher(VIEW).forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String userName = request.getParameter(CHAMP_NOM);
-        String pwd = request.getParameter(CHAMP_PASS);
-        
-        try {
-			String resp = Connection.getHTML("http://etud.insa-toulouse.fr/~kurzaj/?uid=" + userName + "&pwd=" + pwd);
+		String pwd = request.getParameter(CHAMP_PASS);
+
+		try {
+			String resp = Connection.sendPost("http://etud.insa-toulouse.fr/~kurzaj/", "uid=" + userName + "&pwd=" + pwd);
 			System.out.println("resp:" + resp);
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Could not connect to the LDAP proxy server !");
 		}
 	}
-	
+
 	public static String getHTML(String urlToRead) throws Exception {
-	      StringBuilder result = new StringBuilder();
-	      URL url = new URL(urlToRead);
-	      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-	      conn.setRequestMethod("GET");
-	      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-	      String line;
-	      while ((line = rd.readLine()) != null) {
-	         result.append(line);
-	      }
-	      rd.close();
-	      return result.toString();
-	   }
+		StringBuilder result = new StringBuilder();
+		URL url = new URL(urlToRead);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String line;
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+		rd.close();
+		return result.toString();
+	}
+
+	public static String sendPost(String url, String urlParameters) throws Exception {
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// add request header
+		con.setRequestMethod("POST");
+
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		// System.out.println("\nSending 'POST' request to URL : " + url);
+		// System.out.println("Post parameters : " + urlParameters);
+		// System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		return response.toString();
+	}
 
 }
