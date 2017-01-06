9c082a74adb79128b232e2f6a9c7ee725c85a7ab
Wastalar <djebli@etud.insa-toulouse.fr>
2017-01-06 15:46:38 +0100

    add to gitignore

--- Internships/src/fr/insa/modele/LM_.java
+++ Internships/src/fr/insa/modele/LM_.java
@@ -5,12 +5,12 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2016-12-16T15:43:47.341+0100")
+@Generated(value="Dali", date="2016-12-16T17:23:38.584+0100")
 @StaticMetamodel(LM.class)
 public class LM_ {
 	public static volatile SingularAttribute<LM, Long> id;
 	public static volatile SingularAttribute<LM, Date> dateUpload;
+	public static volatile SingularAttribute<LM, Etudiant> etudiant;
 	public static volatile SingularAttribute<LM, String> nom;
 	public static volatile SingularAttribute<LM, String> urlStock;
-	public static volatile SingularAttribute<LM, Etudiant> etudiant;
 }
--- Internships/src/fr/insa/modele/Stage_.java
+++ Internships/src/fr/insa/modele/Stage_.java
@@ -5,14 +5,14 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2016-12-16T15:45:44.477+0100")
+@Generated(value="Dali", date="2016-12-16T17:23:34.121+0100")
 @StaticMetamodel(Stage.class)
 public class Stage_ {
 	public static volatile SingularAttribute<Stage, Long> id;
 	public static volatile SingularAttribute<Stage, Date> debut;
-	public static volatile SingularAttribute<Stage, String> description;
 	public static volatile SingularAttribute<Stage, String> titre;
 	public static volatile SingularAttribute<Stage, String> lieu;
 	public static volatile SingularAttribute<Stage, Integer> duree;
+	public static volatile SingularAttribute<Stage, String> description;
 	public static volatile SingularAttribute<Stage, Entreprise> entreprise;
 }
