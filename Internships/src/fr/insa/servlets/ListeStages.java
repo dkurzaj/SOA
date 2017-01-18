9fc2c9c6e728bf8d1c7108e62708e42a8a50020c
Nicolas Perez <git@nicolasperez.com>
2017-01-18 17:44:36 +0100

    Affichage seulement si un utilisateur est loggué

--- Internships/src/fr/insa/servlets/ListeStages.java
+++ Internships/src/fr/insa/servlets/ListeStages.java
@@ -8,11 +8,13 @@ import java.sql.Date;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import fr.insa.modele.Etudiant;
 import fr.insa.modele.Stage;
@@ -20,21 +22,26 @@ import fr.insa.modele.Stage;
 @WebServlet("/liste-stages")
 public class ListeStages extends HttpServlet {
 	public static final String VUE = "/WEB-INF/liste-stages.jsp";
-
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory( "Internships" );
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-
-		entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-        List<Stage> liste = entityManager.createQuery( "from Stage", Stage.class ).getResultList();
-		entityManager.getTransaction().commit();
-        entityManager.close();
-		
-		request.setAttribute("liste", liste);
-		
-		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+		HttpSession session = request.getSession();
+		if (session.getAttribute("sessionNomUtilisateur") == null) {
+			//Si on n'est pas connecté
+			response.sendRedirect(request.getContextPath() + "/connexion");
+		} else {
+			//Si on est connecté
+			EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory( "Internships" );
+			EntityManager entityManager = entityManagerFactory.createEntityManager();
+	
+			entityManager = entityManagerFactory.createEntityManager();
+			entityManager.getTransaction().begin();
+	        List<Stage> liste = entityManager.createQuery( "from Stage", Stage.class ).getResultList();
+			entityManager.getTransaction().commit();
+	        entityManager.close();
+			
+			request.setAttribute("liste", liste);
+			
+			this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+		}
 	}
 }
